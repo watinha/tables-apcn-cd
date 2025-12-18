ea44d2ab2fb8262f22fa4a798f42fe6662c4d245
Willian Massami Watanabe <watinha@gmail.com>
2025-12-18 15:27:24 -0300

    refactoring the code to make it extensible

--- lib/__init__.py
+++ lib/__init__.py
@@ -8,7 +8,7 @@ def read_orientadores_file(file_path):
     return orientadores
 
 
-def generate_orientacoes_table (df, orientadores):
+def _generate_orientadores_query (df, orientadores):
     informadaPor = df['Informada por'].unique()
     orientadores_query = []
     for orientador in orientadores:
@@ -18,22 +18,36 @@ def generate_orientacoes_table (df, orientadores):
                 orientador_list.append(informada)
         orientadores_query.append(orientador_list)
 
-    tipo_de_orientacoes = df['Tipo da produção'].unique()
-    orientacoes_data = []
+    return orientadores_query
+
+
+def _get_prod_by_orientador (df, orientadores):
+    prod_by_orientador = {}
+
+    orientadores_query = _generate_orientadores_query(df, orientadores)
 
     for orientador in orientadores_query:
         query = None
         orientacoes_rows = []
 
-        if isinstance(orientador, list):
-            query = df['Informada por'] == orientador[0]
-            for i in range(1, len(orientador)):
-                query |= df['Informada por'] == orientador[i]
-            orientador_df = df[query]
-            orientacoes_rows.append(orientador[0])
-        else:
-            orientador_df = df[df['Informada por'] == orientador]
-            orientacoes_rows.append(orientador)
+        query = df['Informada por'] == orientador[0]
+        for i in range(1, len(orientador)):
+            query |= df['Informada por'] == orientador[i]
+        orientador_df = df[query]
+
+        prod_by_orientador[orientador[0]] = orientador_df
+
+    return prod_by_orientador
+
+
+def generate_orientacoes_table (df, orientadores):
+    prod_by_orientador = _get_prod_by_orientador(df, orientadores)
+    tipo_de_orientacoes = df['Tipo da produção'].unique()
+    orientacoes_data = []
+
+    for orientador in orientadores:
+        orientacoes_rows = [orientador]
+        orientador_df = prod_by_orientador[orientador]
 
         for tipo in tipo_de_orientacoes:
             count = orientador_df[orientador_df['Tipo da produção'] == tipo].shape[0]

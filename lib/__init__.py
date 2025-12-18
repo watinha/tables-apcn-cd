8898c0f980d173ffa662278b3b86a577a09e80fb
Willian Massami Watanabe <watinha@gmail.com>
2025-12-18 17:17:55 -0300

    reporting technical production

--- lib/__init__.py
+++ lib/__init__.py
@@ -77,10 +77,61 @@ def generate_bibliographic_prod_table (df, orientadores):
             else:
                 orientador_row.append(0)
 
+        outros = orientador_df[orientador_df['Tipo da produção'] != 'Artigo publicado em periódicos']
+        outros_sorted = ['Trabalho publicado em anais de evento', 'Organização de obra publicada', 'Capítulo de livro publicado', 'Livro publicado']
+        outros_count = outros['Tipo da produção'].value_counts()
+        for outro_tipo in outros_sorted:
+            if (outro_tipo in outros_count.keys().tolist()):
+                orientador_row.append(int(outros_count[outro_tipo]))
+            else:
+                orientador_row.append(0)
+
         bibliographic_prod_data.append(orientador_row)
 
-    return pd.DataFrame(bibliographic_prod_data, columns=['Orientador', 'A1', 'A2', 'A3', 'A4', 'B1', 'B2', 'B3', 'B4', 'C', 'NP'])
+    return pd.DataFrame(bibliographic_prod_data, columns=['Orientador', 'A1', 'A2', 'A3', 'A4', 'B1', 'B2', 'B3', 'B4', 'C', 'NP', 'Trabalhos publicados em anais de eventos', 'Organização de obra publicada', 'Capítulo de livro publicado', 'Livro publicado'])
     
+
+def generate_prod_tec_table (df, orientadores):
+    prod_by_orientador = _get_prod_by_orientador(df, orientadores)
+    tipo_de_producao = df['Tipo da produção'].unique()
+    prod_tec_data = []
+
+    for orientador in orientadores:
+        orientador_row = [orientador]
+        orientador_df = prod_by_orientador[orientador]
+
+        tipos_tec = [
+            'Apresentação de Trabalho e palestra',
+            'Trabalhos técnicos',
+            'Programa de computador'
+        ]
+
+        prod_tec_counts = orientador_df['Tipo da produção'].value_counts()
+        for tipo_tec in tipos_tec:
+            if (tipo_tec in prod_tec_counts.keys().tolist()):
+                orientador_row.append(int(prod_tec_counts[tipo_tec]))
+            else:
+                orientador_row.append(0)
+
+        prod_tec_data.append(orientador_row)
+
+
+    return pd.DataFrame(prod_tec_data, columns=['Orientador'] + tipos_tec)
+
+
+#def generate_prod_table_for_all (df_bib, df_tec, orientadores):
+#    bib_prod_por_orientador = _prod_by_orientador(df_bib, orientadores)
+#    tec_prod_por_orientador = _prod_by_orientador(df_tec, orientadores)
+#
+#    for orientador in orientadores:
+#        bib_df = bib_prod_por_orientador[orientador]
+#        tec_df = tec_prod_por_orientador[orientador]
+#    
+#        
+
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-from lib import generate_orientacoes_table, read_orientadores_file, generate_bibliographic_prod_table
+from lib import generate_orientacoes_table, read_orientadores_file, generate_bibliographic_prod_table, generate_prod_tec_table
 
 
 if __name__ == "__main__":
@@ -17,4 +17,8 @@ if __name__ == "__main__":
     prod_by_orientadores.to_excel('./results/bibliographic_production_table.xlsx', index=False)
     print("Bibliographic Production table generated successfully!")
 
+    df = pd.read_excel('./data/producao-tecnica.xlsx', skiprows=[0, 1, 2])
+    prod_by_orientadores = generate_prod_tec_table(df, orientadores)
+    prod_by_orientadores.to_excel('./results/producao-tecnica.xlsx', index=False)
+    print("Bibliographic Production table generated successfully!")
     
